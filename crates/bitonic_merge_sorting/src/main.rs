4085495eb08e220c806413c525636fe843d4dec3
dsmtE <desmet.enguerrand@gmail.com>
2023-03-28 01:06:30 +0200

    ✨global BMS taking workgroups size into account

--- crates/bitonic_merge_sorting/src/main.rs
+++ crates/bitonic_merge_sorting/src/main.rs
@@ -40,7 +40,7 @@ fn main() {
 
 	println!("Unsorted: {:?}", vec);
 
-	bitonic_merge_sort(&mut vec, workgroup_size_x);
+	global_bitonic_merge_sort(&mut vec, workgroup_size_x);
 
 	println!("Sorted: {:?}", vec);
 
@@ -48,59 +48,156 @@ fn main() {
 }
 
 fn local_compare_and_swap(vec: &mut Vec<u32>, a: usize, b: usize) {
+	print!("[{},{}]", a, b);
+	if vec[a] > vec[b] {
+		vec.swap(a, b)
+	}
+}
+
+// here not real difference between local and global compare and swap
+// This will be used in the future when we will have a global memory and a local memory
+fn global_compare_and_swap(vec: &mut Vec<u32>, a: usize, b: usize) {
 	print!("[{}, {}] ", a, b);
 	if vec[a] > vec[b] {
 		vec.swap(a, b)
 	}
 }
 
-fn do_flip(vec: &mut Vec<u32>, t:usize, h: usize) {
-	let q = ((2 * t) / h) * h;
+fn local_flip(vec: &mut Vec<u32>, h:usize, workgroup_size_x: usize) {
+	print!("Local flip over height {} ", h);
+	let n = vec.len();
+	let workgroup_count = n / ( workgroup_size_x * 2 );
 	let half_h = h / 2;
-	local_compare_and_swap(
-		vec,
-		q + t % half_h,
-		q + h - (t % half_h) - 1
-	);
+
+	// Simulate workgroups and barrier using loops
+	for workgroup_id in 0..workgroup_count {
+		print!(" |wg({})| ", workgroup_id);
+		let h_offset = h * ( ( workgroup_size_x * workgroup_id * 2 ) / h );
+
+		for local_thread_id in 0..workgroup_size_x {
+			let q = h_offset + ((2 * local_thread_id) / h) * h;
+			
+			local_compare_and_swap(
+				vec,
+				q + local_thread_id % half_h,
+				q + h - (local_thread_id % half_h) - 1
+			);
+		}
+	}
+	println!("");
 }
 
-fn do_disperse(vec: &mut Vec<u32>, t: usize, h: usize) {
-	let q = ((2 * t) / h) * h;
+fn big_flip(vec: &mut Vec<u32>, h: usize, workgroup_size_x: usize) {
+	print!("Big Flip over height {} ", h);
+
+	let n = vec.len();
+	let workgroup_count = n / ( workgroup_size_x * 2 );
 	let half_h = h / 2;
-	local_compare_and_swap(vec,
-		q + t % half_h,
-		q + (t % half_h) + half_h
-	);
+
+	// TODO: rewrite using macro to avoid code duplication
+	// Simulate workgroups and barrier using loops
+	for workgroup_id in 0..workgroup_count {
+		print!(" |wg({})| ", workgroup_id);
+		for local_thread_id in 0..workgroup_size_x {
+			
+			let global_thread_id = workgroup_id * workgroup_size_x + local_thread_id;
+			let q = ( ( 2 * global_thread_id ) / h ) * h;
+
+			global_compare_and_swap(
+				vec,
+				q + ( global_thread_id % half_h ),
+				q + h - ( global_thread_id % half_h ) - 1
+			);
+		}
+	}
+	println!("");
+}
+
+fn local_disperse(vec: &mut Vec<u32>, h: usize, workgroup_size_x: usize) {
+	print!("local Disperse over height {} ", h);
+	let n = vec.len();
+	let workgroup_count = n / ( workgroup_size_x * 2 );
+	let half_h = h / 2;
+
+	for workgroup_id in 0..workgroup_count {
+		print!(" |wg({})| ", workgroup_id);
+		let h_offset = h * ( ( workgroup_size_x * workgroup_id * 2 ) / h );
+
+		for local_thread_id in 0..workgroup_size_x {
+			let q = h_offset + ((2 * local_thread_id) / h) * h;
+
+			let x = q + local_thread_id % half_h;
+			local_compare_and_swap(vec,
+				x,
+				x + half_h
+			);
+		}
+	}
+	println!("");
+}
+
+fn big_disperse(vec: &mut Vec<u32>, h: usize, workgroup_size_x: usize) {
+	print!("Big Disperse over height {} | ", h);
+	//assert!(workgroup_size_x * 2 > h, "number of sortable elements processed by one workgroup must be smaller or equal to flip height");
+	let n = vec.len();
+	let workgroup_count = n / ( workgroup_size_x * 2 );
+	let half_h = h / 2;
+
+	for workgroup_id in 0..workgroup_count {
+		print!(" |wg({})| ", workgroup_id);
+		for local_thread_id in 0..workgroup_size_x {
+			let global_thread_id = workgroup_id * workgroup_size_x + local_thread_id;
+			let q = ( ( 2 * global_thread_id ) / h ) * h;
+
+			let x = q + (global_thread_id % half_h);
+			global_compare_and_swap(vec,
+				x,
+				x + half_h
+			);
+		}
+	}
+	println!("");
 }
 
 // Use vec here as shared memory
-fn bitonic_merge_sort(vec: &mut Vec<u32>, workgroup_size_x: usize) {
-	let mut h = 2;
-	let n = vec.len();
+fn local_bitonic_merge_sort(vec: &mut Vec<u32>, h: usize, workgroup_size_x: usize) {
+	println!("local BMS height: {} Workgroup size: {}", h, workgroup_size_x);
 
-	println!("{} elements, Workgroup size: {}", n, workgroup_size_x);
-	while h <= n {
-
-		// this loop normally would be a barrier
-		print!("Flip over height {} | ", h);
-		for t in 0..workgroup_size_x {
-			do_flip(vec, t, h);
+	let mut hh = 2;
+	while hh <= h {
+		local_flip(vec, hh, workgroup_size_x);
+		let mut hhh = hh / 2;
+		while hhh > 1 {
+			local_disperse(vec, hhh, workgroup_size_x);
+			hhh /= 2;
 		}
-		println!("");
+		hh *= 2;
+	}
+}
+
+fn global_bitonic_merge_sort(vec: &mut Vec<u32>, workgroup_size_x: usize) {
+	let n = vec.len();
+	println!("global BMS {} elements, Workgroup size: {}", n, workgroup_size_x);
+	let mut h = workgroup_size_x * 2;
+
+	local_bitonic_merge_sort(vec, h, workgroup_size_x);
+
+	// we must now double h, as this happens before every flip
+	h *= 2;
+	while h <= n {
+		big_flip( vec, h, workgroup_size_x);
 
 		let mut hh = h / 2;
+		
 		while hh > 1 {
-
-			// this loop normally would be a barrier
-			print!("Disperse over height {} | ", hh);
-			for t in 0..workgroup_size_x {
-				do_disperse(vec, t, hh);
+			if hh <= workgroup_size_x * 2 {
+				local_disperse(vec, hh, workgroup_size_x);
+			} else {
+				big_disperse(vec, hh, workgroup_size_x);
 			}
-			println!("");
-			
 			hh /= 2;
 		}
+
 		h *= 2;
 	}
-
 }
